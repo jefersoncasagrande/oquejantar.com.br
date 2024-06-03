4c8077fc25c862c67ce25c918cd2e99835312507
Jeferson Casagrande <jefe.casagrande@gmail.com>
2024-06-03 19:38:07 -0300

    Correção API

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Gemini\Laravel\Facades\Gemini;
 
 Route::get('/get-dinner', function(Request $request) {
-    $client = Gemini::client(getenv('GEMINI_API_KEY'));
-    $result = $client->geminiPro()->generateContent('Sugestão para jantar. apenas nome de 1 prato. ');
+    // $result = Gemini::geminiPro()->generateContent('Sugestão para jantar. apenas nome de 1 prato. ');
+    $result = Gemini::generativeModel('gemini-1.5-flash')->generateContent('Sugestão para jantar. apenas nome de 1 prato. ');
     return [$result->text()];
+});
+
+Route::get('/models', function(Request $request) {
+    $result = Gemini::models()->list();
+    return [$result->models];
 });
